de8d6e03c7c156944883f99ae3dbd89d4a29cc85
Justen <justenkhilliard@gmail.com>
2026-07-03 23:53:57 -0400

    Add useState and click handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 function App() {
+  const [clicked, setClicked] = useState(false);
+
+  function handleClick() {
+    setClicked(!clicked);
+  }
+
   return (
     <div>
       <h1>Justen Hilliard</h1>
       <p>Just practicing react and stuff booooom</p>
+
+      <button onClick={handleClick}>
+        {clicked ? "you clicked it lol" : "click me"}
+      </button>
     </div>
   )
 }
